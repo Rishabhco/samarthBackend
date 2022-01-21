0dd6e473cd874d5e55b9512b4082198ebd94a09b
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-01-22 01:53:26 +0530

    added job routes ✨

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
 const express=require('express')
 const auth=require('../middleware/auth.js')
 const userController=require('../controller/userController.js')
+const jobController=require('../controller/jobController.js')
 const router=new express.Router()
 
 router.get('/',userController.home);
 router.post('/signup',userController.createUser);
 router.post('/login',userController.login);
 router.post('/logout',auth,userController.logout);
+
+router.get('/jobHome',jobController.home);
+router.post('/createJob',auth,jobController.createJob);
+router.get('/findAllJob',jobController.findAllJob);
+router.post('/findAllJobCat',jobController.findAllJobCat);
 
 module.exports=router
